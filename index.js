54ac5bba65cbde6ac4fe10c2a79ca147a172d9c3
Eity2021 <Eity2021akter@gmail.com>
2025-01-31 20:39:13 +0600

    data upddated

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
+const cors = require('cors'); 
 const express = require('express');
-const cors = require('cors'); // Import CORS
 const projects = require('./projects');
 
+app.use(cors());
 const app = express();
 const port = 3001;
 
 
-app.use(cors());
 
 
 app.use(
