e620c8a60756d606bca9091a43802d489f68a2a6
Eity2021 <Eity2021akter@gmail.com>
2025-01-31 20:33:22 +0600

    cors issue

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express');
-const port = 3000;
+const cors = require('cors'); // Import CORS
 const projects = require('./projects');
 
 const app = express();
+const port = 3001;
+
+
+app.use(cors());
+
+
+app.use(
+  cors({
+    origin: "http://localhost:3001", 
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
+    credentials: true, 
+  })
+);
 
 app.use(express.json());
+
+// Use projects route
 app.use('/projects', projects);
 
 app.listen(port, () => {
