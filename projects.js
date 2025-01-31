42f721d8e71f625926c53500eb3446612f5eac2b
Eity2021 <Eity2021akter@gmail.com>
2025-01-31 19:30:41 +0600

    add data

--- projects.js
+++ projects.js
@@ -13,33 +13,33 @@ router.get('/:id', (req, res) => {
   res.json(projects.filter((ele) => ele.id === parseInt(id)));
 });
 
-router.post('/', (req, res) => {
-  const body = req.body;
-  console.log(body);
-  projects.push(body);
-  res.json({ message: 'The book has been added' });
-});
+// router.post('/', (req, res) => {
+//   const body = req.body;
+//   console.log(body);
+//   projects.push(body);
+//   res.json({ message: 'The book has been added' });
+// });
 
-router.put('/:id', (req, res) => {
-  const { id } = req.params;
-  const body = req.body;
-  projects.forEach((project, index) => {
-    if (project.id === parseInt(id)) {
-      projects[index] = body;
-    }
-  });
-  res.json({ message: `The book with ID ${id} has been updated` });
-  // res.json(books);
-});
+// router.put('/:id', (req, res) => {
+//   const { id } = req.params;
+//   const body = req.body;
+//   projects.forEach((project, index) => {
+//     if (project.id === parseInt(id)) {
+//       projects[index] = body;
+//     }
+//   });
+//   res.json({ message: `The book with ID ${id} has been updated` });
+//   // res.json(books);
+// });
 
-router.delete('/:id', (req, res) => {
-  const { id } = req.params;
-  projects.forEach((project, index) => {
-    if (project.id === parseInt(id)) {
-      projects.splice(index);
-    }
-  });
-  res.json({ message: `Book with id #${id} has been deleted` });
-});
+// router.delete('/:id', (req, res) => {
+//   const { id } = req.params;
+//   projects.forEach((project, index) => {
+//     if (project.id === parseInt(id)) {
+//       projects.splice(index);
+//     }
+//   });
+//   res.json({ message: `Book with id #${id} has been deleted` });
+// });
 
 module.exports = router;
