31a0bb9bae6b8a472b8a0f97d3b6f169dd1b4c91
Eity2021 <Eity2021akter@gmail.com>
2025-02-01 20:34:41 +0600

    dataa upp

--- projects.js
+++ projects.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const router = express.Router();
 const projects = require('./projects.json');
 
-// Enable CORS for this route
+
 router.use(
   cors({
     origin: "http://localhost:3000", 
@@ -12,12 +12,11 @@ router.use(
   })
 );
 
-// Get all projects
+
 router.get('/', (req, res) => {
   res.json(projects);
 });
 
-// Get a specific project
 router.get('/:id', (req, res) => {
   const { id } = req.params;
   res.json(projects.filter((ele) => ele.id === parseInt(id)));
